4d5404e0af297d9e9282cc5e46b918d9ae2d76b1
Vincent Tu <vincenttu@gheldmandare.com>
2023-06-05 20:23:08 -0700

    6-5-2023

---
+++ source/conf.py
@@ -0,0 +1,25 @@
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Ruckus Unleashed Wireless Documentation'
+# copyright = '2023, Vincent Tu'
+# author = 'Vincent Tu'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_copybutton']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'default'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. Ruckus Wireless Unleashed documentation master file, created by
+   sphinx-quickstart on Mon Jun  5 20:14:43 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Ruckus Wireless Unleashed's documentation!
+=====================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
